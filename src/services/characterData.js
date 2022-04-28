b2fbf64d0fe96edd25375fe11e05bbe7df6ad9f1
Cole Rossman <colerossman99@gmail.com>
2022-04-28 14:28:04 -0700

    characterData file added to servies with mock data and setupTests file added server to be universally used

---
+++ src/services/characterData.js
@@ -0,0 +1,22 @@
+export const user = {
+    id: 1,
+    created_at: '2021-12-13T00:17:29+00:00',
+    // 🚨 Add a name here
+    name: 'Johnny',
+    avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+    header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+    likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+    motto: 'Res Non Verba',
+    color: 'crimson',
+  }
+
+  export const sasuke = {
+    id: 1,
+    created_at: '2021-12-13T00:17:29+00:00',
+    name: 'Sasuke 🌬️🔥',
+    avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+    header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+    likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+    motto: 'Res Non Verba',
+    color: 'crimson',
+  }
